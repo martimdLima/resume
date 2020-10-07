dd52207a25e42e2b3340398b168e51e18f175c29
Martim Lima <martim.d.lima@protonmail.com>
2020-10-07 13:34:12 +0100

    Updates

---
+++ precache-manifest.e6b68341e057577f856967a3f1b4d4c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32587250342daa0c0c77ccdf295432b5",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "d66be94cf5339d7557ef",
+    "url": "/resume/static/css/main.6c0fbcbd.chunk.css"
+  },
+  {
+    "revision": "cc36b045786f297c6eea",
+    "url": "/resume/static/js/2.c08e30cc.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/resume/static/js/2.c08e30cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d66be94cf5339d7557ef",
+    "url": "/resume/static/js/main.3c74c8c0.chunk.js"
+  },
+  {
+    "revision": "8c1d914a1677cea53979",
+    "url": "/resume/static/js/runtime-main.75d8c91e.js"
+  }
+]);
